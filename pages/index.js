def1ac3190639c1d673aed09b09de1a925a898a9
Periastrum <Uwehensel@live.de>
2025-09-01 11:49:04 +0200

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{textAlign:"center",marginTop:"50px",fontFamily:"Arial, sans-serif"}}>
+      <h1>Willkommen bei Tauschitauschi 🎉</h1>
+      <p>Die Tausch-Plattform entsteht gerade – bald kannst du hier loslegen!</p>
+    </div>
+  );
+}
